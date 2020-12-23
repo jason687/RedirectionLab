5ceadbe71d1002d2c00df5dfec7f0da05b2d86fe
jason687 <jzhang20@stuy.edu>
2020-12-23 13:11:51 -0500

    first attempt

---
+++ MakeStars.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MakeStars {
+  public static void main (String [] args) {
+    Scanner n = new Scanner (System.in);
+    String line = "";
+    String word = "";
+    String outputStuff = "";
+    while (n.hasNextLine()) {
+      line = n.nextLine();
+      Scanner o = new Scanner (line);
+      while (o.hasNext()) {
+        word = o.next();
+        outputStuff += starify(word) + " ";
+      }
+      outputStuff += "/n";
+      System.out.println(outputStuff);
+    }
+  }
+
+  public static String starify (String a) {
+    int len = a.length();
+    String str = "";
+    for (int i = 0; i < len; i++) {
+      str += "*";
+    }
+    return str;
+  }
+}
