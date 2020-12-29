03c30e539d551fa17e01d33c85e6e52860341cbc
jason687 <jzhang20@stuy.edu>
2020-12-28 20:27:28 -0500

    oops forgot to commit things...

---
+++ PigLatin.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class PigLatin {
+
+  public static String pigLatinSimple (String s) {
+    if ((s.charAt(0) == 'a') || (s.charAt(0) == 'e') || (s.charAt(0) == 'i') || (s.charAt(0) == 'o') || (s.charAt(0) == 'u')) {
+      return s + "hay";
+    }
+    return s.substring(1) + s.charAt(0) + "ay";
+  }
+
+  public static void main (String [] args) {
+    System.out.println(pigLatinBest ("mock"));
+    System.out.println(pigLatinBest ("apple"));
+    System.out.println(pigLatinBest ("check"));
+    System.out.println(pigLatinBest ("check."));
+  }
+
+  public static String pigLatin (String s) {
+    s = s.toLowerCase();
+    String [] Digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < Digraph.length; i++) {
+      if (s.substring(0, 2).equals(Digraph[i])) {
+        return s.substring(2) + s.substring(0, 2) + "ay";
+      }
+    }
+    return pigLatinSimple(s);
+  }
+
+  public static String pigLatinBest (String s) {
+    String punct = "";
+    if ((s.charAt(0) < 'a') || (s.charAt(0) > 'Z')) {
+      return s;
+    }
+    if (!Character.isLetterOrDigit(s.charAt(s.length() - 1))) {
+      punct += (s.charAt(s.length() - 1));
+      s = s.substring(0, s.length() - 1);
+    }
+    s = pigLatin(s);
+    s += punct;
+    return s;
+  }
+}
