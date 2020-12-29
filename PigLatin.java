350b6eafa84c6613bad486b02ac06979090ac863
jason687 <jzhang20@stuy.edu>
2020-12-28 21:41:55 -0500

    i got the java file to compile, but i am not sure if it functions properly... it looks about right when i skim the outputs, but the diff method is telling me otherwise...

--- PigLatin.java
+++ PigLatin.java
@@ -10,18 +10,11 @@ public class PigLatin {
     return s.substring(1) + s.charAt(0) + "ay";
   }
 
-  public static void main (String [] args) {
-    System.out.println(pigLatinBest ("mock"));
-    System.out.println(pigLatinBest ("apple"));
-    System.out.println(pigLatinBest ("check"));
-    System.out.println(pigLatinBest ("check!"));
-  }
-
   public static String pigLatin (String s) {
     s = s.toLowerCase();
     String [] Digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     for (int i = 0; i < Digraph.length; i++) {
-      if (s.substring(0, 2).equals(Digraph[i])) {
+      if (s.length() > 2 && s.substring(0, 2).equals(Digraph[i])) {
         return s.substring(2) + s.substring(0, 2) + "ay";
       }
     }
@@ -40,5 +33,22 @@ public class PigLatin {
     s = pigLatin(s);
     s += punct;
     return s;
+  }
+
+  public static void main (String [] args) {
+    Scanner n = new Scanner (System.in);
+    String line = "";
+    String word = "";
+    String outputStuff = "";
+    while (n.hasNextLine()) {
+      line = n.nextLine();
+      Scanner o = new Scanner (line);
+      while (o.hasNext()) {
+        word = o.next();
+        outputStuff += pigLatinBest(word) + " ";
+      }
+      outputStuff += "\b\n";
+    }
+    System.out.print(outputStuff);
   }
 }
